be6ff787f52bbdf549ba72fde5bc0d10f128c40a
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 18:27:53 +0300

    g

--- exercises/superheros.py
+++ exercises/superheros.py
@@ -1,1 +1,11 @@
 # Write your solutions for 1.5 here!
+
+class superheroes :
+    def __init__(self,name,superpower,streangth):
+        self.name=name
+        self.superpower=superpower
+        self.streangth=streangth
+    def superid(self):
+        print(self.name,self.streangth)
+dr_strange=superheroes("dr.strange","magic", 9001)
+dr_strange.superid()
